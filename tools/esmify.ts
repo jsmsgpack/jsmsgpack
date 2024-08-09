5ab2e251a577427cb9247d2f1afa51403affd2a7
晓明 王 <wxm@v1i.cc>
2024-08-09 17:01:41 +0800

    fix: 修复windows下npm install相关问题

--- tools/esmify.ts
+++ tools/esmify.ts
@@ -2,23 +2,37 @@
 /* eslint-disable no-console */
 
 import fs from "fs";
+import glob from "glob";
 
-const files = process.argv.slice(2);
+const patterns = process.argv.slice(2);
 
-for (const file of files) {
-  const fileMjs = file.replace(/\.js$/, ".mjs");
-  console.info(`Processing ${file} => ${fileMjs}`);
-  // .js => .mjs
-  const content = fs.readFileSync(file).toString("utf-8");
-  const newContent = content.replace(/\bfrom "(\.\.?\/[^"]+)";/g, 'from "$1.mjs";')
-    .replace(/\/\/# sourceMappingURL=(.+)\.js\.map$/,
-      "//# sourceMappingURL=$1.mjs.map");
-  fs.writeFileSync(fileMjs, newContent);
-  fs.unlinkSync(file);
+patterns.forEach(pattern => {
+  glob(pattern, (err: any, files: any) => {
+    if (err) {
+      console.error(`Error processing pattern: ${pattern}`, err);
+      process.exit(1);
+    }
 
-  // .js.map => .mjs.map
-  const mapping = JSON.parse(fs.readFileSync(`${file}.map`).toString("utf-8"));
-  mapping.file = mapping.file.replace(/\.js$/, ".mjs");
-  fs.writeFileSync(`${fileMjs}.map`, JSON.stringify(mapping));
-  fs.unlinkSync(`${file}.map`);
-}
+    for (const file of files) {
+      const fileMjs = file.replace(/\.js$/, ".mjs");
+      console.info(`Processing ${file} => ${fileMjs}`);
+
+      // .js => .mjs
+      const content = fs.readFileSync(file).toString("utf-8");
+      const newContent = content.replace(/\bfrom "(\.\.?\/[^"]+)";/g, 'from "$1.mjs";')
+          .replace(/\/\/# sourceMappingURL=(.+)\.js\.map$/,
+              "//# sourceMappingURL=$1.mjs.map");
+      fs.writeFileSync(fileMjs, newContent);
+      fs.unlinkSync(file);
+
+      // .js.map => .mjs.map
+      const mapFile = `${file}.map`;
+      if (fs.existsSync(mapFile)) {
+        const mapping = JSON.parse(fs.readFileSync(mapFile).toString("utf-8"));
+        mapping.file = mapping.file.replace(/\.js$/, ".mjs");
+        fs.writeFileSync(`${fileMjs}.map`, JSON.stringify(mapping, null, 2));
+        fs.unlinkSync(mapFile);
+      }
+    }
+  });
+});
